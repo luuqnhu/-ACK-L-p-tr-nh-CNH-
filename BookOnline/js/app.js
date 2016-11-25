f8adbc60b56e488e82a062203b3d89bbebe8686d
NakunMinh <nakunminh@gmail.com>
2016-11-25 11:21:06 +0700

    Thiết lập cấu trúc cơ bản

---
+++ BookOnline/js/app.js
@@ -0,0 +1,29 @@
+var myApp = angular.module('myApp', ['ngRoute']);
+myApp.config(function($routeProvider){
+	$routeProvider
+	.when('/', {
+		templateUrl: 'home.html'
+	})
+	.when('/login', {
+		templateUrl: 'login.html'
+	})
+	.when('/register', {
+		templateUrl: 'register.html'
+	})
+	.when('/detail', {
+		templateUrl: 'detail.html'
+	})
+	.when('/cart', {
+		templateUrl: 'cart.html'
+	})
+	.when('/add_cart_success', {
+		templateUrl: 'add_cart_success.html'
+	})
+	.when('/thank', {
+		templateUrl: 'thank.html'
+	});
+});
+
+myApp.controller('bookonline', function($scope){
+
+});
