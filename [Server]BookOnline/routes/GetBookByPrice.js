c6e1ce0c9a8e4f92537f84287efd8b951652e007
LuuNhu\Luu Nhu <luu.qnhu@gmail.com>
2016-12-31 17:36:31 +0700

    [Server] API thống kê sách

---
+++ [Server]BookOnline/routes/GetBookByPrice.js
@@ -0,0 +1,46 @@
+/**
+ * Created by Luu Nhu on 12/31/2016.
+ */
+var express   =    require("express");
+var router = express.Router();
+var mysql     =    require('mysql');
+
+var pool      =    mysql.createPool({
+    connectionLimit : 100, //important
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'bookonline',
+    debug    :  false
+});
+
+function handle_database(req,res) {
+
+    pool.getConnection(function(err,connection){
+        if (err) {
+            res.json({"code" : 100, "status" : "Error in connection database"});
+            return;
+        }
+
+        console.log('connected as id ' + connection.threadId);
+
+        connection.query("Select * from sach where Gia = ?", [req.params.Gia],function(err,rows){
+            connection.release();
+            if(!err) {
+                //setValue(rows);
+                res.json(rows);
+            }
+        });
+
+        connection.on('error', function(err) {
+            res.json({"code" : 100, "status" : "Error in connection database"});
+            return;
+        });
+    });
+}
+
+router.get("/:Gia",function(req,res){
+    handle_database(req,res);
+});
+
+module.exports = router;
